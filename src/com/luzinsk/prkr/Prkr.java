10973ab69ccf88fdb5e87f3a14f8f8d0d2db4a32
Ethan <ethanppilz@gmail.com>
2020-05-01 19:59:37 -0400

    first

---
+++ src/com/luzinsk/prkr/Prkr.java
@@ -0,0 +1,18 @@
+package com.luzinsk.prkr;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Prkr extends JavaPlugin {
+
+    @Override
+    public void onEnable(){
+
+    }
+
+    @Override
+    public void onDisable(){
+
+    }
+
+
+}
