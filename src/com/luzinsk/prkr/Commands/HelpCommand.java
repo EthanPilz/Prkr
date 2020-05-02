a3e41f49835f1b85cf3f881e6c27844a9ef5c6b8
Ethan <ethanppilz@gmail.com>
2020-05-01 20:17:13 -0400

    added help command and registry to prkr

---
+++ src/com/luzinsk/prkr/Commands/HelpCommand.java
@@ -0,0 +1,25 @@
+package com.luzinsk.prkr.Commands;
+
+import org.bukkit.ChatColor;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+
+public class HelpCommand implements CommandExecutor {
+
+    private static final String prkrPrefix = ChatColor.DARK_GRAY + "[" + ChatColor.YELLOW + "Prkr" + ChatColor.DARK_GRAY + "] " + ChatColor.YELLOW;
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args){
+
+        if (sender.hasPermission("prkr.help")){
+            sender.sendMessage(ChatColor.YELLOW + "" + ChatColor.STRIKETHROUGH + "--------------------" + ChatColor.GREEN + "" + ChatColor.BOLD + " Prkr "
+                    + ChatColor.RESET + ChatColor.YELLOW + "" + ChatColor.STRIKETHROUGH + "--------------------");
+            sender.sendMessage(prkrPrefix + "we will fill this shit in laterrrrrr");
+        }
+        return true;
+    }
+
+
+
+}
--- src/com/luzinsk/prkr/Prkr.java
+++ src/com/luzinsk/prkr/Prkr.java
@@ -1,16 +1,28 @@
 package com.luzinsk.prkr;
 
+import com.luzinsk.prkr.Commands.HelpCommand;
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
 import org.bukkit.plugin.java.JavaPlugin;
+
+import java.util.logging.Level;
 
 public class Prkr extends JavaPlugin {
 
+    private static final String prkrPrefix = ChatColor.AQUA + "[Prkr] ";
+
     @Override
     public void onEnable(){
+
+
+        getCommand("help").setExecutor(new HelpCommand());
 
     }
 
     @Override
     public void onDisable(){
+
+        Bukkit.getLogger().log(Level.INFO, prkrPrefix + ChatColor.RED + "Prkr disabled.");
 
     }
 
