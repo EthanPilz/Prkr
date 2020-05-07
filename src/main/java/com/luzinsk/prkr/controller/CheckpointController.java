afe2c248420a7aa53da0650c64549528c93e3d54
Carlton <hiimbaconsu@gmail.com>
2020-05-06 23:01:08 -0400

    +checkpoints stored in config

--- src/main/java/com/luzinsk/prkr/controller/CheckpointController.java
+++ src/main/java/com/luzinsk/prkr/controller/CheckpointController.java
@@ -1,5 +1,7 @@
 package com.luzinsk.prkr.controller;
 
+import com.luzinsk.prkr.Prkr;
+import org.bukkit.Bukkit;
 import org.bukkit.Location;
 import org.bukkit.entity.Player;
 
@@ -8,6 +10,7 @@ import java.util.Optional;
 
 public class CheckpointController {
     private HashMap<String, Location> checkpoints;
+    private Prkr plugin = Prkr.getPlugin(Prkr.class);
 
     public CheckpointController(){
         checkpoints = new HashMap<>();
@@ -16,12 +19,35 @@ public class CheckpointController {
 
     public void registerCheckpoint(Player player, Location location){
         checkpoints.put(player.getUniqueId().toString(), location);
+
+        plugin.getConfig().set("Player Checkpoints." + player.getUniqueId().toString() + ".Yaw", location.getYaw());
+        plugin.getConfig().set("Player Checkpoints." + player.getUniqueId().toString() + ".Pitch", location.getPitch());
+        plugin.getConfig().set("Player Checkpoints." + player.getUniqueId().toString() + ".X", location.getX());
+        plugin.getConfig().set("Player Checkpoints." + player.getUniqueId().toString() + ".Y", location.getY());
+        plugin.getConfig().set("Player Checkpoints." + player.getUniqueId().toString() + ".Z", location.getZ());
+        plugin.getConfig().set("Player Checkpoints." + player.getUniqueId().toString() + ".World", location.getWorld().getName());
+        plugin.saveConfig();
     }
 
     public Optional<Location> getCheckpoint(Player player){
+
         if(checkpoints.containsKey(player.getUniqueId().toString())) {
+
             return Optional.of(checkpoints.get(player.getUniqueId().toString()));
-        } else {
+
+        } else if (plugin.getConfig().contains("Player Checkpoints." + player.getUniqueId().toString())) {
+
+            float yaw = plugin.getConfig().getInt("Player Checkpoints." + player.getUniqueId().toString() + ".Yaw");
+            float pitch = plugin.getConfig().getInt("Player Checkpoints." + player.getUniqueId().toString() + ".Pitch");
+            double x = (Double) plugin.getConfig().getDouble("Player Checkpoints." + player.getUniqueId().toString() + ".X");
+            double y = (Double) plugin.getConfig().getDouble("Player Checkpoints." + player.getUniqueId().toString() + ".Y");
+            double z = (Double) plugin.getConfig().getDouble("Player Checkpoints." + player.getUniqueId().toString() + ".Z");
+            String wrld = plugin.getConfig().getString("Player Checkpoints." + player.getUniqueId().toString() + ".World");
+
+            Location checkpointLoc = new Location(Bukkit.getWorld(wrld), x, y, z, yaw, pitch);
+            return Optional.of(checkpointLoc);
+        }
+        else {
             return Optional.empty();
         }
     }
